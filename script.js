eee1678db7ee14781747492f0db0272d41ca3d62
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-09-09 23:04:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name = name;
+		this.age = age;
+	}
+	get name(){
+		return this.name;
+	}
+	get age(){
+		return this.age;
+	}
+	set age(newage){
+		if(newage>0){
+			this.age=newage;
+		}else{
+			console.log("Age must be positive");
+		}
+	}
+}
+class Student extends Person {
+	constructor(name,age){
+		super(name,age);
+	}
+	study(){
+		return (this.name + " is studying");
+	}
+}
 
-class Student extends Person {}
-
-class Teacher extends Person {}
+class Teacher extends Person {
+	constructor(name,age){
+		super(name,age);
+	}
+	Teaching(){
+		return (this.name + " is teaching");
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
